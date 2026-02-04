bbcca2c64accd641e1120dddab92cee8eeeb039c
giovannimazzi <gioelor@libero.it>
2026-02-04 16:53:32 +0100

    step 5: inserita struttura di base Main, tolto fragment dove ridondante

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,11 @@
 export default function Main() {
-  return <main> MAIN </main>;
+  return (
+    <main className="bg-dark text-white">
+      <div className="container py-5">
+        <p className="h2">
+          {"--> "}Content goes here{" <--"}
+        </p>
+      </div>
+    </main>
+  );
 }
