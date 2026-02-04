2b7b54ce5ad01d17368711a3b80d8134853799e2
giovannimazzi <gioelor@libero.it>
2026-02-04 22:18:06 +0100

    step 8: aggiunte animazioni navbar header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <header>
       <div className="container">
-        <div className="d-flex justify-content-between align-items-center p-3">
-          <img src={logo} alt="DC Comics Logo" />
+        <div className="d-flex justify-content-between align-items-stretch">
+          <img src={logo} alt="DC Comics Logo" className="p-3" />
 
           <nav>
-            <ul className="d-flex gap-3 list-unstyled m-0 fw-bold">
-              <li>
+            <ul className="h-100 d-flex gap-3 list-unstyled m-0 fw-bold">
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a
                   className="text-decoration-none text-dark"
                   href="#CHARACTERS"
@@ -17,27 +17,30 @@ export default function Header() {
                   CHARACTERS
                 </a>
               </li>
-              <li>
-                <a className="text-decoration-none text-dark" href="#COMICS">
+              <li className="h-100 d-flex flex-column justify-content-center">
+                <a
+                  className="text-decoration-none text-dark active"
+                  href="#COMICS"
+                >
                   COMICS
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a className="text-decoration-none text-dark" href="#MOVIES">
                   MOVIES
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a className="text-decoration-none text-dark" href="#TV">
                   TV
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a className="text-decoration-none text-dark" href="#GAMES">
                   GAMES
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a
                   className="text-decoration-none text-dark"
                   href="#COLLECTIBLES"
@@ -45,22 +48,22 @@ export default function Header() {
                   COLLECTIBLES
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a className="text-decoration-none text-dark" href="#VIDEOS">
                   VIDEOS
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a className="text-decoration-none text-dark" href="#FANS">
                   FANS
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a className="text-decoration-none text-dark" href="#NEWS">
                   NEWS
                 </a>
               </li>
-              <li>
+              <li className="h-100 d-flex flex-column justify-content-center">
                 <a className="text-decoration-none text-dark" href="#SHOP">
                   SHOP
                 </a>
