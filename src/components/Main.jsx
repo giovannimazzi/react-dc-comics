1aa494166d47d24b7dc14c87009840efefea3e0e
giovannimazzi <gioelor@libero.it>
2026-02-05 16:39:40 +0100

    step 15: creati componenti per la struttura del Main e inserito Banner

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
+import Banner from "./Banner";
+import ComicsList from "./ComicsList";
+
 export default function Main() {
   return (
     <main className="flex-grow-1 text-white">
+      <Banner />
       <div className="container py-5">
-        <p className="h2">
-          {"--> "}Content goes here{" <--"}
-        </p>
+        <ComicsList />
       </div>
     </main>
   );
