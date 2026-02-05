efa67a9474ec946dcc9840b8b44a646b87c88278
giovannimazzi <gioelor@libero.it>
2026-02-05 12:58:31 +0100

    step 11: corretta la struttura dei link di BlueStrip

--- src/components/BlueStrip.jsx
+++ src/components/BlueStrip.jsx
@@ -4,65 +4,47 @@ import subscriptions from "../assets/img/buy-comics-subscriptions.png";
 import shopLocator from "../assets/img/buy-comics-shop-locator.png";
 import powerVisa from "../assets/img/buy-dc-power-visa.svg";
 
+const links = [
+  {
+    url: "#",
+    src: digital,
+    text: "DIGITAL COMICS",
+  },
+  {
+    url: "#",
+    src: merchandise,
+    text: "DC MERCHANDISE",
+  },
+  {
+    url: "#",
+    src: subscriptions,
+    text: "SUBSCRIPTION",
+  },
+  {
+    url: "#",
+    src: shopLocator,
+    text: "COMIC SHOP LOCATOR",
+  },
+  {
+    url: "#",
+    src: powerVisa,
+    text: "DC POWER VISA",
+  },
+];
+
 export default function BlueStrip() {
   return (
     <section className="blue-strip">
       <div className="container">
         <ul className="d-flex justify-content-between align-items-center list-unstyled m-0 p-5">
-          <li>
-            <a
-              href="#DIGITALCOMICS"
-              aria-label="Digital Comics"
-              className="blue-link"
-            >
-              <img src={digital} alt="" />
-              <span>DIGITAL COMICS</span>
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#DCMERCHANDISE"
-              aria-label="DC Merchandise"
-              className="blue-link"
-            >
-              <img src={merchandise} alt="" />
-              <span>DC MERCHANDISE</span>
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#SUBSCRIPTION"
-              aria-label="Subscription"
-              className="blue-link"
-            >
-              <img src={subscriptions} alt="" />
-              <span>SUBSCRIPTION</span>
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#COMICSHOPLOCATOR"
-              aria-label="Comic Shop Locator"
-              className="blue-link"
-            >
-              <img src={shopLocator} alt="" />
-              <span>COMIC SHOP LOCATOR</span>
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#DCPOWERVISA"
-              aria-label="DC Power Visa"
-              className="blue-link"
-            >
-              <img src={powerVisa} alt="" />
-              <span>DC POWER VISA</span>
-            </a>
-          </li>
+          {links.map(({ url, src, text }) => (
+            <li>
+              <a href={url} aria-label={text} className="blue-link">
+                <img src={src} alt="" />
+                <span>{text}</span>
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
